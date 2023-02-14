459ce9a677522454b7dba88a382da45531cb1b2f
MakenaG <makenaglory44@gmail.com>
2023-02-14 23:23:29 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
-
+    # setter
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+    #  getter
+    def name
+        @this_dogs_name
+    end
 end
+
+pico = Dog.new
+pico.name ="fino"
+puts pico.name
